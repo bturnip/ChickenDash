4f6e3177aa3d5a51b4b5061b1011c78ca7717625
bturnip <bturnip@bturnip.com>
2015-12-24 22:41:58 -0600

    Adding files for 1st time

---
+++ cdd.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Danny Anderson
+20151017
+
+@author: bturnip
+"""
+
+import cdd_meta as meta
+#import google_calendar_api as google
+import csv
+import datetime
+from datetime import date
+from astral import Astral
+import os
+from scapy.all import *
+
+
+# =========================================================================
+# Variables
+# =========================================================================
+FILE = meta.CDD_DATA_FILE
+LOG_FILE = meta.CDD_DIR + "/data/cdd_log_" + meta.YYYYMMDD + ".log"
+CURRENT_TIME = meta.TIMEZONE.localize(datetime.datetime.now())
+DEBUG_FLAG = meta.DEBUG_FLAG
+
+# =========================================================================
+# Prep logging
+# =========================================================================
+with open(LOG_FILE, 'w') as LOG:
+    meta.WRITE_LOG(LOG_FILE, "ChickenDoorDash starting", True)
+
+
+# =========================================================================
+# Astral Variables
+# =========================================================================
+city_name = meta.CITY
+a = Astral()
+a.solar_depression = 'civil'
+city = a[city_name]
+timezone = city.timezone
+sun = city.sun(date=date.today(), local=True)
+
+meta.WRITE_LOG(LOG_FILE,
+               'Astral calculation for Sunset: ' + str(sun['sunset']),
+               True)
+meta.WRITE_LOG(LOG_FILE,
+               'Astral calculation for Dusk:   ' + str(sun['dusk']),
+               True)
+
+SECONDS_TIL_SUNSET = meta.SECONDS_BETWEEN(sun['sunset'])
+SECONDS_TIL_DUSK = meta.SECONDS_BETWEEN(sun['dusk'])
+DUSK_TIME = sun['dusk']
+
+
+# =========================================================================
+# Main loop
+# =========================================================================
+# Once program starts, go into a listening loop until the dash button
+# has been pushed OR dusk has arrived
+# =========================================================================
+i = 0
+if(DEBUG_FLAG):
+    # if DEBUGGING, allow 25 pings, regardless of time
+    while (not meta.IS_DASH_BUTTON_01_PUSHED and i < 25):
+        i += 1
+        print sniff(prn=meta.arp_display, filter="arp", store=0, count=meta.NUM_SNIFFS)
+
+        print "sniff run #" + str(i), "CURRENT_TIME:", CURRENT_TIME, "IS_CDD_BUTTON_PUSHED", meta.IS_DASH_BUTTON_01_PUSHED
+
+    print "LOOP EXIT", "Button Push:", meta.IS_DASH_BUTTON_01_PUSHED, \
+    "CURRENT_TIME", CURRENT_TIME
+else:
+    while (not meta.IS_DASH_BUTTON_01_PUSHED and CURRENT_TIME < DUSK_TIME):
+        i += 1
+        print sniff(prn=meta.arp_display, filter="arp", store=0, count=meta.NUM_SNIFFS)
+
+        CURRENT_TIME = meta.TIMEZONE.localize(datetime.datetime.now())
+        print "sniff run #" + str(i), "CURRENT_TIME:", CURRENT_TIME, "IS_CDD_BUTTON_PUSHED", meta.IS_DASH_BUTTON_01_PUSHED
+
+        print "LOOP EXIT", "Button Push:", meta.IS_DASH_BUTTON_01_PUSHED, \
+        "CURRENT_TIME", CURRENT_TIME
+
+
+
+
+
+
+
+
+
+
+#start at 5pm
+#does log exist?
+#    yes- append
+#    no - create
+
+#calcuate dusk print sniff(prn=arp_display, filter="arp", store=0, count=NUM_SNIFFS)time
+
+#start checking for ping
+#if ping:
+    #record and quit
+#if dusk and not ping
+    #record and notify
+    #wait 10 mins
+    #record and notify
+    #wait 10 mins
+    #record and notify
+
+
+#This program uses the sniff() callback (paramter prn). The store parameter is set to 0 so that the sniff() function will not store anything (as it would do otherwise) and thus can run forever. The filter parameter is used for better performances on high load : the filter is applied inside the kernel and Scapy will only see ARP traffic.
+
+##! /usr/bin/env python
+#from scapy.all import *
+
+#def arp_monitor_callback(pkt):
+    #if ARP in pkt and pkt[ARP].op in (1,2): #who-has or is-at
+        #return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
+
+#sniff(prn=arp_monitor_callback, filter="arp", store=0)
+
+
+
+
+
+
+
---
+++ cdd_meta.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Danny Anderson
+20151017
+
+@author: bturnip
+"""
+import datetime
+import os
+import pytz
+from scapy.all import *
+
+# =========================================================================
+# Variables
+# =========================================================================
+DEBUG_FLAG = True
+today = datetime.datetime.today()
+YYYYMMDD = today.strftime('%Y%m%d%H%M%S')
+CITY = "Little Rock"
+# Kludge, though this comes out automatically
+# using Astral if the city is recognized
+TIMEZONE = pytz.timezone('US/Central')
+
+
+# =========================================================================
+# Button Variables
+# =========================================================================
+DASH_BTTN_MAC_01 = '74:75:48:24:39:fd'
+DASH_BTTN_MAC_02 = ''
+
+
+NUM_SNIFFS = 10
+IS_DASH_BUTTON_01_PUSHED = False
+
+
+# =========================================================================
+# Location
+# =========================================================================
+pass
+
+
+# =========================================================================
+# File paths
+# =========================================================================
+CDD_DIR = "/home/bturnip/Documents/Code/python/ChickenDash"
+CDD_DATA_FILE = CDD_DIR + "data/cdd.csv"
+
+
+# =========================================================================
+# Needlessly complicated versioning
+# =========================================================================
+version = '0.1'
+
+v = int(version[0])
+
+avatar_dict = {
+    4:"Perching Penny"
+    ,3:"Sassy Sally"
+    ,2:"Irresistible Imogene"
+    ,1:"Nesting Nugget"
+    ,0:"Roaming Road Runner"}
+
+avatar = avatar_dict[v]
+
+
+# =========================================================================
+# For DEBUG
+# =========================================================================
+#print "+++DEBUG:", "DASH_BTTN_MAC",DASH_BTTN_MAC
+#print "+++DEBUG:", "avatar",avatar
+#print "+++DEBUG:", "version",version
+#print "+++DEBUG:", "CDD_DIR",CDD_DIR
+#print "+++DEBUG:", "CDD_DATA",CDD_DATA
+#print "+++DEBUG:", "CDD_DATA_FILE",CDD_DATA_FILE
+
+
+# =========================================================================
+# Logging function
+# =========================================================================
+def WRITE_LOG(file, message, print_option = False):
+    lognow = datetime.datetime.today()
+    logdate = lognow.strftime('%c')
+
+    # strip one or more newlines (we'll add one later ourselves)
+    while (message[-1] == "\n"):
+        message = message[:-1]
+
+    log_message = logdate + "|" + message + "\n"
+
+    if os.path.isfile(file):
+        with open(file, "a") as f:
+            f.write(log_message)
+    else:
+        print "+++ERROR", "WRITE_LOG(file, message):", file, message, " failed!"
+
+    if print_option:
+        print log_message.rstrip(os.linesep)
+
+
+# =========================================================================
+# Calculate time diffs
+# =========================================================================
+def SECONDS_BETWEEN(event_time):
+
+    RIGHT_NOW = TIMEZONE.localize(datetime.datetime.now())
+    # print "+++DEBUG: RIGHT_NOW:", RIGHT_NOW
+
+    # we passed in a date, right?
+    if isinstance(event_time, datetime.datetime):
+        if (event_time >= RIGHT_NOW):
+            print "+++INFO: SECONDS_BETWEEN(): future event"
+            print (event_time - RIGHT_NOW).total_seconds()
+            return int((event_time - RIGHT_NOW).total_seconds())
+        else:
+            print "+++INFO: SECONDS_BETWEEN(): past event"
+            print (RIGHT_NOW - event_time).total_seconds()
+            return int((RIGHT_NOW - event_time).total_seconds())
+    else:
+        print "+++ERROR: YOU ARE KILLING ME, SMALLS!!"
+
+
+# =========================================================================
+def arp_display(pkt):
+# =========================================================================
+    global IS_DASH_BUTTON_01_PUSHED
+
+    try:
+        if pkt[ARP].op == 1:
+            if pkt[ARP].psrc == '0.0.0.0':
+                if pkt[ARP].hwsrc == DASH_BTTN_MAC_01:
+                    print "Pushed Dash Button"
+                    IS_DASH_BUTTON_01_PUSHED = True
+                else:
+                    print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+            else:
+                pass
+
+    except IndexError as e:
+        print e
+
